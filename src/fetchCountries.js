3c9cbebeced15f3472a90cd4551757c725a1c762
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-06-08 21:42:32 +0300

    next

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,14 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 export function fetchCountries(name) {
-    
-};
+  const parametr = 'name,capital,population,flags,languages';
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=${parametr}
+`).then(response => {
+    if (!response.ok) {
+      throw new Error(
+        Notify.failure('Oops, there is no country with that name')
+      );
+    }
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,56 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
-import {fetchCountries} from './fetchCountries'
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+inputEl.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+const countryList = document.querySelector('.country-list');
+const countryContejner = document.querySelector('.country-info');
+
+function onInputSearch(ev) {
+  onClearInput();
+  const input = ev.target.value.trim();
+  fetchCountries(input).then(response => {
+    if (response.length === 1) {
+      countryCardRender(response);
+    }
+    if (response.length >= 2 && response.length <= 10) {
+      countryListRender(response);
+    }
+    if (response.length > 10) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+    }
+  });
+}
+
+function onClearInput() {
+  countryList.innerHTML = '';
+  countryContejner.innerHTML = '';
+}
+
+function countryCardRender(response) {
+  const markup = response
+    .map(element => {
+      return `<div class="item"><img class="img" src="${
+        element.flags.svg
+      }" width=50 alt="flag"><h1 class = "title">${
+        element.name.official
+      }</h1></div><p class="text"><b>Capital:</b> ${element.capital}</p>
+<p class="text"><b>Population:</b> ${element.population}</p>
+<p class="text"><b>Languages:</b> ${Object.values(element.languages)}</p>`;
+    })
+    .join('');
+  countryContejner.innerHTML = markup;
+}
+
+function countryListRender(response) {
+  const markup = response
+    .map(element => {
+      return `<li class="item"><img class="img" src="${element.flags.svg}"  width=30 alt="flag"><h3 class = "title">${element.name.official}</h3></li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+}
